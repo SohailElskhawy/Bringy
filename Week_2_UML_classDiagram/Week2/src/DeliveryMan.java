24d9bb96f786bb07389442c1c590bf723b1a49a0
ibrhmzyd <ibrh>
2025-03-18 20:12:06 +0300

    added Week2

---
+++ Week_2_UML_classDiagram/Week2/src/DeliveryMan.java
@@ -0,0 +1,8 @@
+import java.util.*; 
+public class DeliveryMan {
+    private List<Order> orderList; 
+
+    public List<Order> getAllOrders() {
+        return null; 
+    }
+}
---
+++ Week_2_UML_classDiagram/Week2/src/User.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public abstract class User {
+
+    public String name; 
+    public int id; 
+    public String email; 
+    private String password; 
+    public String role; 
+
+    public boolean login(){
+        // ... 
+        return true;
+    }
+
+    public void logout(){
+
+    }
+    public void updateProfile(){
+        
+    }
+      
+} 
